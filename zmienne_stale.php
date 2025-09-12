baf1f46ba04237f2ce0dd73c7944685596d648b4
Antoni344199 <darkstormy335@gmail.com>
2025-09-12 10:36:09 +0200

    886

---
+++ zmienne_stale.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php 
+#zadanie 1
+$ciag_znakow = 123456
+$liczba_calkowita = -5555555
+$liczba_zmiennoprzecinkowa = 0.50050505
+$bol = true
+
+#zadanie 2
+<br>
+echo $ciag_znakow
+<br>
+var_dump $liczba_calkowita
+<br>
+echo $liczba_zmiennoprzecinkowa
+<br>
+var_dump $bol
+
+#zadanie 3
+define ('czoko')
+define ("koczo")
+
+print czoko
+var_export koczo
+
+
+?>
+<p> </p>
+
+
+</body>
+</html>
