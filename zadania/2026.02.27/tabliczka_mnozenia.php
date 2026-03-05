93292391a02440beec21bda727d313fac24c2c3f
abrozyna <abrozynakielce@gmail.com>
2026-03-05 18:56:23 +0100

    praca domowa - tabliczka mnozenia

--- zadania/2026.02.27/tabliczka_mnozenia.php
+++ zadania/2026.02.27/tabliczka_mnozenia.php
@@ -9,12 +9,22 @@
 <body>
     
     <table>
-<!-- petla ma robic 10 razy row z kazdym mnozeniem  -->
-        <?php for($a=0;$a<=10;$a++)
-                if($a==0)
-                    echo "<td>x</td>";
-                else
-                    echo "<td>$a</td>";
+        <?php 
+        for ($c=0;$c<=10;$c++)
+            if ($c == 0)
+                echo "<td>X</td>";
+            else
+                echo "<th>$c</th>";
+
+        for($a=1;$a<=10;$a++){
+                echo "<tr><th>$a</th>";
+                for($b=1;$b<=10;$b++)
+                    if($a*$b == $a * $a)
+                        echo "<td class='srodek'>".($b * $a)."</td>";
+                    else
+                        echo "<td>".($b * $a)."</td>";
+
+                echo "</tr>";}
 
         ?>
     </table>
