cea1ba79680bdc5829f8460d0e37819b3bbf0fab
abrozyna <abrozynakielce@gmail.com>
2026-03-05 17:42:32 +0100

    poprawka

--- zadania/2026.02.20/plyta_glowna_atx.php
+++ zadania/2026.02.20/plyta_glowna_atx.php
@@ -9,10 +9,15 @@
 <?php    
     $str0= "Płyta główna ATX";
     $str = mb_str_split($str0, 1);
+    
     foreach($str as $str1)
-        if($str1 == "a" || $str1 == "A")
-            $color= 'MAGENTA';
-            echo $str1
+        if($str1 === "a" || $str1 === "A") 
+           echo  "<span style='color:magenta;'>$str1</span>";
+        
+        else
+        echo $str1;
+        
+    
 ?>
 </body>
 </html>
