ed79857601dd47fb4a599acf713c815fd9a5647e
abrozyna <abrozynakielce@gmail.com>
2026-04-23 20:33:14 +0200

    homework bitches !!!!

--- zadania/2026.04.17/formularz_zamowienia.php
+++ zadania/2026.04.17/formularz_zamowienia.php
@@ -14,6 +14,7 @@
     $erremail = '';
     $errdodat = '';
     $errzgoda = '';
+    $blad = False;
     if($_SERVER['REQUEST_METHOD'] == 'POST')
         if(empty($_POST['nazwt']))
             $errnazwa = 'Podaj nazwę produktu';
@@ -26,14 +27,14 @@
 
     <label  for="nazwt">Nazwa towaru</label><span class=star> *</span><br>
     <input type="text" name="nazwt" id="nazwt" <?php if($_SERVER['REQUEST_METHOD'] == 'POST')
-        if(empty($_POST['nazwt'])) echo 'class="error"' ?>><br><span class="star"><?= $errnazwa ?></span><br>
+        if(empty($_POST['nazwt'])){ echo 'class="error"'; $blad = True;} ?>><br><span class="star"><?= $errnazwa ?></span><br>
 
 
     
     <label for="opcja_pakowania">Wybierz opcje pakowania:</label><span class=star> *</span><br>
 
-    <div <?php if($_SERVER['REQUEST_METHOD'] == 'POST')
-        if(empty($_POST['opcja_pakowania'])) echo 'class="error"'; ?>
+    <div <?php if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if(empty($_POST['opcja_pakowania'])){ echo 'class="error"' ; $blad = True;}}?>
         >
 
     <input type="checkbox" name="opcja_pakowania" value="koperta" id="koperta">
@@ -49,12 +50,17 @@
     <label for="karton">karton</label><br>
 
     <input type="checkbox" name="opcja_pakowania" value="karton z usztywnieniem" id="kartonplus">
-    <label for="kartonplus">karton z usztywnieniem</label></div><br>
-    <?= $errpakowanie ?>
+    <label for="kartonplus">karton z usztywnieniem</label>
+    
+    </div>
+    <?php if($_SERVER['REQUEST_METHOD'] == 'POST')
+        if(empty($_POST['opcja_pakowania'])) echo '<span class="star">Wybierz opcję pakowania</span>'; ?><br>
     <br>
 
 
-
+    <div <?php if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if(empty($_POST['pakowanie'])){ echo 'class="error"'; $blad = True;}} ?>
+        >
     <label for="pakowanie">Podaj wagę paczki:</label><span class=star> *</span><br>
 
     <input type="radio" name="pakowanie" value="2kg" id="2kg">
@@ -68,27 +74,63 @@
 
     <input type="radio" name="pakowanie" value="od10do15kg" id="od10do15kg">
     <label for="od10do15kg">od 10 do 15 kg</label><br>
+    </div>
+    <?php if($_SERVER['REQUEST_METHOD'] == 'POST')
+            if(empty($_POST['pakowanie'])) echo '<span class="star">Określ przybliżoną wagę paczki</span>'; ?>
+    <br>
 
 
 
     <label for="email">Email kontaktowy:</label><span class=star> *</span><br>
-    <input type="email" name="email" id="email"><br><br>
+    <input class="<?php if($_SERVER['REQUEST_METHOD'] == 'POST')
+        if(empty($_POST['email'])) echo 'error' ?>" type="email" name="email" id="email"><br>
+        <?php if($_SERVER['REQUEST_METHOD'] == 'POST')
+        if(empty($_POST['email'])){ echo '<span class="star">Podaj poprawny adres email</span>'; $blad = True;} ?>
+        <br>
 
 
 
     <label for="info">Dodatkowe informacje</label><br>
-    <textarea name="info" id="info"></textarea><br><br>
+    <textarea class="<?php if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                            if(strlen($_POST['info']) < 15){ echo 'error'; $blad = True;}} ?>" name="info" id="info">
+    </textarea><br>
+        <?php if($_SERVER['REQUEST_METHOD'] == 'POST')
+                            if(strlen($_POST['info']) < 15) echo '<span class="star">Wiadomość musi mieć conajmniej 15 znaków</span>' ?>
+        
+                            <br>
 
 
 
     <input type="checkbox" name="przdane" id="przdane" value="zgodanadane">
-    <label for="przdane">Zgoda na przetwarzanie danych</label><span class=star> *</span><br><br><br>
+    <label for="przdane">Zgoda na przetwarzanie danych</label><span class=star> *</span><br>
+    <?php if($_SERVER['REQUEST_METHOD'] == 'POST')
+            if(empty($_POST['przdane'])){ echo '<span class="star">Potwierdź swoją zgodę</span>'; $blad = True;} ?>
+    <br><br>
 
 
 
-    <button type="submit" value="wyslij">Wyślij</button>
+    <button type="submit" value="wyslij">Wyślij</button><br>
+    
+
+    <?php 
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if($blad == false){
+            echo '<span class="star">Dane z formularza zostały zapisane do pliku formularz_zamowienia.txt</span>';
+            $filename = 'formularz_zamowienia.txt';
+                if(file_exists($filename)){
+                    $file_handle = fopen($filename, 'a');}
+                    else{
+                        $file_handle = fopen($filename, 'w');}
+                    $danedopliku = date('Y/m/d H:i:s') . "\n" . print_r($_POST, true);
+                    fwrite($file_handle, $danedopliku);
+                    fclose($file_handle);
+                    
+
+                
+        }
+    }
+        ?>
+    
     </form>
-
-    <?php print_r($_POST) ?>
 </body>
 </html>
